8a66ec599e13c70eb85e4fb0008a9b564c0530b2
Korney Saushkin <saukorn03@gmail.com>
2024-06-27 18:26:02 +0200

    Add fields of the class, Getter, Setter and Constructor

--- ShopDemoP/src/main/java/com/demop/ShopDemoP/domain/User.java
+++ ShopDemoP/src/main/java/com/demop/ShopDemoP/domain/User.java
@@ -1,4 +1,26 @@
 package com.demop.ShopDemoP.domain;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String password;
+    private String email;
+    private String Name;
+
+
 }
