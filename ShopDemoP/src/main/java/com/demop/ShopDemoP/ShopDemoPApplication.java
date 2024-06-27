ab4a84f2cdd72716527c2c09ff830910af00322a
Korney Saushkin <saukorn03@gmail.com>
2024-06-27 15:41:04 +0200

    Initial commit

---
+++ ShopDemoP/src/main/java/com/demop/ShopDemoP/ShopDemoPApplication.java
@@ -0,0 +1,13 @@
+package com.demop.ShopDemoP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDemoPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopDemoPApplication.class, args);
+	}
+
+}
---
+++ ShopDemoP/src/test/java/com/demop/ShopDemoP/ShopDemoPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demop.ShopDemoP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopDemoPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
