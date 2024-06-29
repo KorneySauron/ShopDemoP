c1cf5b5afdf2d23bfa6635c1e443dd2162e1be2b
Korney Saushkin <saukorn03@gmail.com>
2024-06-29 19:13:00 +0200

    Add registration.html, add springSecurity dependency, add registerUser method in UserController, add findBy- methods in UserRepository, add save and find methods in UserService, move application.yaml.

--- ShopDemoP/src/main/java/com/demop/ShopDemoP/controller/UserController.java
+++ ShopDemoP/src/main/java/com/demop/ShopDemoP/controller/UserController.java
@@ -1,9 +1,29 @@
 package com.demop.ShopDemoP.controller;
 
+import com.demop.ShopDemoP.domain.User.User;
+import com.demop.ShopDemoP.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/registration")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+        if (userService.findByUsername(user.getName()) != null) {
+            return "Username already exists!";
+        }
+        if (userService.findByEmail(user.getEmail()) != null) {
+            return "Email already exists!";
+        }
+        userService.saveUser(user);
+        return "User registered successfully!";
+    }
 
 }
